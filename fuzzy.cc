c38b7c0c95e0a0699092ff646672cfca765c2631
Mateusz Malinowski <mm429561@students.mimuw.edu.pl>
2021-11-08 13:44:14 +0100

    do końca labów

--- fuzzy.cc
+++ fuzzy.cc
@@ -1,1 +1,42 @@
 #include "fuzzy.h"
+
+using namespace std;
+
+//TriFuzzyNumSet::TriFuzzyNumSet(const TriFuzzyNumSet &other) {
+//
+//}
+//
+//TriFuzzyNumSet &TriFuzzyNumSet::operator=(const TriFuzzyNumSet &other) {
+//    return ;
+//}
+//
+//TriFuzzyNumSet::TriFuzzyNumSet(const TriFuzzyNumSet &&other) {
+//
+//}
+//
+//TriFuzzyNumSet &TriFuzzyNumSet::operator=(TriFuzzyNumSet &&other) {
+//    return ;
+//}
+void TriFuzzyNumSet::insert(const TriFuzzyNum &n) {
+    s.insert(n);
+}
+
+void TriFuzzyNumSet::insert(TriFuzzyNum &&n) {
+    s.insert(n);
+}
+
+void TriFuzzyNumSet::remove(const TriFuzzyNum &n) {
+    s.erase(n);
+}
+
+void TriFuzzyNumSet::remove(TriFuzzyNum &&n) {
+    s.erase(n);
+}
+
+TriFuzzyNum TriFuzzyNumSet::arithmetic_mean() {
+    real_t l = 0, m = 0, u = 0;
+    for (auto n : s) {
+//        sum += n;
+    }
+    return TriFuzzyNum(l, m, u);
+}
--- fuzzy.h
+++ fuzzy.h
@@ -1,8 +1,27 @@
 #ifndef FUZZY_H
 #define FUZZY_H
 
+#include <set>
+
+using real_t = double;
+
 class TriFuzzyNum {
 
+};
+
+class TriFuzzyNumSet {
+    std::multiset<TriFuzzyNum> s;
+
+public:
+    TriFuzzyNumSet(const TriFuzzyNumSet &other) = default;
+    TriFuzzyNumSet &operator=(const TriFuzzyNumSet &other) = default;
+    TriFuzzyNumSet(TriFuzzyNumSet &&other) = default;
+    TriFuzzyNumSet &operator=(TriFuzzyNumSet &&other) = default;
+    void insert(const TriFuzzyNum &n);
+    void insert(TriFuzzyNum &&n);
+    void remove(const TriFuzzyNum &n);
+    void remove(TriFuzzyNum &&n);
+    TriFuzzyNum arithmetic_mean();
 };
 
 #endif //FUZZY_H
