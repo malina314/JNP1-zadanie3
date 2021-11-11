fa13656045d34568c2eeb7372e9bfede8f85c0a6
DilithiumNitrate <38111406+DilithiumNitrate@users.noreply.github.com>
2021-11-11 22:35:42 +0100

    Pierwotna wersja implementacji TriFuzzyNum

--- fuzzy.cc
+++ fuzzy.cc
@@ -1,6 +1,64 @@
 #include "fuzzy.h"
+#include <cmath>
 
 using namespace std;
+
+TriFuzzyNum& TriFuzzyNum::operator+=(const TriFuzzyNum &rhs) {
+    l += rhs.l;
+    m += rhs.m;
+    u += rhs.u;
+    return *this;
+}
+
+TriFuzzyNum& TriFuzzyNum::operator-=(const TriFuzzyNum &rhs) {
+    l -= rhs.l;
+    m -= rhs.m;
+    u -= rhs.u;
+    return *this;
+}
+
+TriFuzzyNum& TriFuzzyNum::operator*=(const TriFuzzyNum &rhs) {
+    l *= rhs.l;
+    m *= rhs.m;
+    u *= rhs.u;
+    return *this;
+}
+
+const TriFuzzyNum TriFuzzyNum::operator+(const TriFuzzyNum &rhs) const {
+    return TriFuzzyNum(*this) += rhs;
+}
+
+const TriFuzzyNum TriFuzzyNum::operator-(const TriFuzzyNum &rhs) const {
+    return TriFuzzyNum(*this) -= rhs;
+}
+
+const TriFuzzyNum TriFuzzyNum::operator*(const TriFuzzyNum &rhs) const {
+    return TriFuzzyNum(*this) *= rhs;
+}
+
+const std::tuple<real_t, real_t, real_t> TriFuzzyNum::getRank() const {
+    real_t z = (u - l) + sqrt(1 + (u - m) * (u - m)) + sqrt(1 + (m - l) * (m - l));
+    real_t y = (u - l) / z;
+    real_t x = ((u - l) * m + sqrt(1 + (u - m) * (u - m)) * l + sqrt(1 + (m - l) * (m - l)) * u) / z;
+    return make_tuple((x-y)/2, 1-y, m);
+}
+
+const std::partial_ordering TriFuzzyNum::operator<=>(const TriFuzzyNum &rhs) const {
+    auto rank1 = getRank();
+    auto rank2 = rhs.getRank();
+
+    auto comp1 = get<0>(rank1) <=> get<0>(rank2);
+    if (comp1 != 0)
+        return comp1;
+
+    auto comp2 = get<1>(rank1) <=> get<1>(rank2);
+    if (comp2 != 0)
+        return comp2;
+    else
+        return get<2>(rank1) <=> get<2>(rank2);
+}
+
+
 
 TriFuzzyNumSet::~TriFuzzyNumSet() {
     s.clear();
--- fuzzy.h
+++ fuzzy.h
@@ -2,11 +2,47 @@
 #define FUZZY_H
 
 #include <set>
+#include <tuple>
 
 using real_t = double;
 
 class TriFuzzyNum {
+    real_t l;
+    real_t m;
+    real_t u;
+    
+    const std::tuple<real_t, real_t, real_t> getRank() const;
+public:
+    constexpr TriFuzzyNum(real_t low, real_t mod, real_t up):
+        l(low), m(mod), u(up) {}
 
+    constexpr TriFuzzyNum(const TriFuzzyNum &other) = default;
+    constexpr TriFuzzyNum(TriFuzzyNum &&other) = default;
+
+    constexpr const real_t lower_value() { return l; }
+    constexpr const real_t modal_value() { return m; }
+    constexpr const real_t upper_value() { return u; }
+
+    TriFuzzyNum &operator=(const TriFuzzyNum &other) = default;
+    TriFuzzyNum &operator=(TriFuzzyNum &&other) = default;
+    
+    TriFuzzyNum& operator+=(const TriFuzzyNum &rhs);
+    TriFuzzyNum& operator-=(const TriFuzzyNum &rhs);
+    TriFuzzyNum& operator*=(const TriFuzzyNum &rhs);
+    
+    const TriFuzzyNum operator+(const TriFuzzyNum &rhs) const;
+    const TriFuzzyNum operator-(const TriFuzzyNum &rhs) const;
+    const TriFuzzyNum operator*(const TriFuzzyNum &rhs) const;
+    
+    const std::partial_ordering operator<=>(const TriFuzzyNum &rhs) const;
+    
+    constexpr const bool operator==(const TriFuzzyNum &rhs) const {
+        return (l == rhs.l) && (m == rhs.m) && (u == rhs.u);
+    }
+
+    constexpr const bool operator!=(const TriFuzzyNum &rhs) const {
+        return !(*this == rhs);
+    }
 };
 
 class TriFuzzyNumSet {
